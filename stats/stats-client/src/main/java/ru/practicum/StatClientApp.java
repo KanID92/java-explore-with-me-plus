0b1826ccbe385b9a36f810f39f3567c28e338af5
Igor_Kanygin <id.mastif@yandex.ru>
2024-08-24 17:05:31 +0300

    - fix;

--- stats/stats-client/src/main/java/ru/practicum/StatClientApp.java
+++ stats/stats-client/src/main/java/ru/practicum/StatClientApp.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StatClientApp {
     public static void main(String[] args) {
+
         SpringApplication.run(StatClientApp.class, args);
+
     }
 }
